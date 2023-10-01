c4f325df78570c07c8752e0a583efa57626b1803
Sandip Bhagat <bhagatsandipk@gmail.com>
2023-10-01 20:15:36 +0530

    comments, commandline args

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 
+/*
+* This is first java program for batch #1
+*/
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World !!");
+        // Above line prints  Hello World !!
     }
 }
